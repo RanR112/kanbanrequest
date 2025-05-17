2cf3f12481b1011dd1c7707ff795e8734f5de684
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-05-17 16:04:45 +0700

    Add Report Monthly

--- kanban-be/src/middlewares/AuthMiddleware.js
+++ kanban-be/src/middlewares/AuthMiddleware.js
@@ -1,6 +1,8 @@
+const prisma = require("../../prisma/client");
 const jwt = require("jsonwebtoken");
 const { sessionSecrets } = require("../controllers/AuthController");
 
+// JWT authentication middleware
 const authenticateToken = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) return res.status(401).json({ 
@@ -10,7 +12,7 @@ const authenticateToken = (req, res, next) => {
 
     try {
         const decodedPayload = jwt.decode(token);
-        const userId = decodedPayload?.id;
+        const userId = decodedPayload?.id_users;
 
         const secret = sessionSecrets[userId];
         if (!secret)
@@ -41,25 +43,61 @@ const authenticateToken = (req, res, next) => {
     }
 };
 
+
+// Middleware to authorize admin users
 const authorizeAdmin = (req, res, next) => {
     if (!req.user) {
         return res.status(401).json({
             success: false,
-            message: 'Access denied. User not authenticated.'
+            message: "Access denied. User not authenticated.",
         });
     }
 
-    if (req.user.role !== 'ADMIN') {
+    if (req.user.role !== "ADMIN") {
         return res.status(403).json({
             success: false,
-            message: 'Access denied. Only ADMIN users can perform this action.'
+            message: "Access denied. Only ADMIN users can perform this action.",
         });
     }
 
     next();
 };
 
+// Middleware to check if user belongs to Department ID 1
+const checkDepartmentAccess = async (req, res, next) => {
+    try {
+        const userId = req.user.id_users; 
+
+        if (!req.user || !userId) {
+            return res.status(401).json({
+                success: false,
+                message: "Access denied. User not authenticated properly.",
+            });
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { id_users: userId },
+            include: { department: true },
+        });
+
+        if (!user || user.id_department !== 1) {
+            return res.status(403).json({
+                success: false,
+                message: "Hanya Department PC yang dapat mengakses laporan",
+            });
+        }
+
+        next();
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Terjadi kesalahan saat memverifikasi akses",
+        });
+    }
+};
+
 module.exports = {
     authenticateToken,
-    authorizeAdmin
+    authorizeAdmin,
+    checkDepartmentAccess,
 };
